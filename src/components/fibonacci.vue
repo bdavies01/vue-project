6bfbf2812670ec37172a124163805f53da9c1716
bdavies01 <bertdaviezz@gmail.com>
2021-02-04 18:16:14 -0800

    Added fibonacci modeule

---
+++ src/components/fibonacci.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ value }}</p>
+    <div>
+      <button @click="execFib">Change</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    prevValue: {
+      type: Number,
+      default: 1
+    }
+  },
+  methods: {
+    execFib() {
+      var newValue = this.value + this.prevValue;
+      this.prevValue = this.value;
+      this.value = newValue;
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <fibonacci />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import fibonacci from "@/components/fibonacci.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    fibonacci
   }
 };
 </script>
